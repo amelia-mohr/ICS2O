abd2446510959dad27795bbb2b8ea3a2f8307557
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-02-27 22:28:01 +0000

    update

--- Unit 1/Unit 1-07/handler.php
+++ Unit 1/Unit 1-07/handler.php
@@ -1,6 +1,25 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Webpage with MDL, Favicon, and PHP form">
+        <meta name="keywords" content="isc2o">
+        <meta name="author" content="Amelia M">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-pink.min.css">
+        <link rel="apple-touch-icon" sizes="180x180" href="./images/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon-16x16.png">
+        <link rel="manifest" href="./images/site.webmanifest">
+        <title>MDL, Favicon, and PHP form</title>
+    </head>
     <body>
-        Welcome <?php echo $_POST["userName"]; ?>
+        <?php
+        $name = "";
+        if ( isset( $_POST["name"] ) ){
+            $name = $_POST["name"];
+        }
+        ?>
     </body>
 </html>
