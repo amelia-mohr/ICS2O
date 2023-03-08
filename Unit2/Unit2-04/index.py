2d7a5c29bb669d5b755e74e2903b893def75fb3d
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-08 18:53:50 +0000

    fix

--- Unit2/Unit2-04/index.py
+++ Unit2/Unit2-04/index.py
@@ -1,10 +1,10 @@
 for numbers in range(1,33): 
     if numbers % 15 == 0.0:
-        print("FizzBuzz")
+        print(str(numbers) + " FizzBuzz")
     elif numbers % 3 == 0.0:
-        print("Fizz")
+        print(str(numbers) + " Fizz")
     elif numbers % 5 == 0.0:
-        print("Buzz")
+        print(str(numbers) + " Buzz")
     else:
         print(numbers)
     
