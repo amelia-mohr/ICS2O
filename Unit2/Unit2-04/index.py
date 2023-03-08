0eca96a9a573332e61be0c23134f52c2bbeec416
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-08 16:47:33 +0000

    fix

--- Unit2/Unit2-04/index.py
+++ Unit2/Unit2-04/index.py
@@ -1,5 +1,9 @@
 x = range(1,33)
 for numbers in x: 
-    if numbers % 3 == 0:
-        numbers = str("Fizz")
+    if numbers % 3 == 0.0:
+        print("Fizz")
+    elif numbers % 5 == 0.0:
+        print("Buzz")
+    else:
+        print(numbers)
     
