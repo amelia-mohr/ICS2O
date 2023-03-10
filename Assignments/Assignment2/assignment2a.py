a9398d67b826186b57dfb134e1d81a10d103d224
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-10 20:53:39 +0000

    fix

--- Assignments/Assignment2/assignment2a.py
+++ Assignments/Assignment2/assignment2a.py
@@ -1,10 +1,10 @@
 import math
 
-x = int(input("Please enter a diameter: "))
-r = x / 2
+d = int(input("Please enter a diameter: "))
+r = d / 2
 area = round(math.pi * (r ** 2), 2)
 circumference = round(2 * math.pi * r, 2)
-print("The diameter of your circle is: " + str(x))
+print("The diameter of your circle is: " + str(d))
 print("The radius of your circle is: " + str(r))
 print("The area of your circle is: " + str(area))
 print("And the circumference of your circle is: " + str(circumference))
