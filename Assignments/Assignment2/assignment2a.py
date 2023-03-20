5d5375938251b7369aef43e57a5faabeef6b93af
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-20 22:15:46 +0000

    fix

--- Assignments/Assignment2/assignment2a.py
+++ Assignments/Assignment2/assignment2a.py
@@ -1,7 +1,7 @@
 import math
 
 d = float(input("Please enter a diameter: "))
-r = round(d / 2)
+r = round(d / 2, 2)
 area = round(math.pi * (r ** 2), 2)
 circumference = round(2 * math.pi * r, 2)
 print("All results are rounded to 2 decimal places.")
