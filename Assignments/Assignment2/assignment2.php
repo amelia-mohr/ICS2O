1ae4482fb3d776e54b4b3d63d40ef3e2b596fc1d
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-23 13:50:24 +0000

    update

--- Assignments/Assignment2/assignment2.php
+++ Assignments/Assignment2/assignment2.php
@@ -8,6 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-pink.min.css">
+        <link rel="stylesheet" href="./css/style2.css">
         <link rel="apple-touch-icon" sizes="180x180" href="./images/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon-16x16.png">
@@ -31,12 +32,37 @@
         settype($a, 'string');
         settype($c, 'string');
         ?>
-        <h1>Assignment #2!</h1>
-        <h3>The area and circumference of a circle!</h3>
-        <h5>The area and circumference are rounded to 2 decimal places.</h5>
-        <p>The diameter of your circle is: <?=$d?></p>
-        <p>The radius of your circle is: <?=$r?></p>
-        <p>The area of your circle is: <?=$a?></p>
-        <p>And the circumference of your circle is: <?=$c?></p>
+
+        <div class="header">
+            <h1 class="title">Assignment #2!</h1>
+        </div>
+        <h3 class="h3">The area and circumference of a circle!</h3>
+        <center>
+            <div class="demo-card-square mdl-card mdl-shadow--2dp">
+                <div class="mdl-card__title mdl-card--border">
+                  <h6 class="mdl-card__title-text h6">All results (except for the diameter) are rounded to 2 decimal places.</h6>
+                </div>
+                <div class="mdl-textfield mdl-js-textfield mdl-card__supporting-text">
+                    <p class="results">
+                    The diameter of your circle is: <?=$d?>
+                    <br>
+                    The radius of your circle is: <?=$r?>
+                    <br>
+                    The area of your circle is: <?=$a?>
+                    <br>
+                    And the circumference of your circle is: <?=$c?>
+                    </p>
+                </div>
+            </div>
+        </center>
+        <br>
+        <p class="para">
+            Link to <a href="https://github.com/amelia-mohr/ICS2O/blob/main/Assignments/Assignment2/assignment2.html">CODE</a> for this (HTML) page.
+            <br>
+            Link to <a href="https://github.com/amelia-mohr/ICS2O/blob/main/Assignments/Assignment2/assignment2.php">CODE</a> for PHP page.
+            <br>
+            Link to <a href="https://github.com/amelia-mohr/ICS2O/blob/main/Assignments/Assignment2/assignment2.py">CODE</a> for Python program.
+        </p>
+        <img src="./images/circle.png" alt="Circle Image" class="img">
     </body>
 </html>
