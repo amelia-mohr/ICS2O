565f26cf368cd806295890cddfa044f35231c031
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-23 20:31:14 +0000

    fix

--- Assignments/Assignment2/assignment2.py
+++ Assignments/Assignment2/assignment2.py
@@ -1,25 +1,12 @@
 import math
-
-while True: 
-    try: 
+pi = math.pi
+d = 0
+while d == 0:
+    try:
         d = float(input("Please enter a diameter: "))
-        if d<0:
-            print("Please enter a POSITIVE number!")
-        elif d==0:
-            print("Please enter a NUMBER!")
-        continue
-    
     except ValueError:
         print("Please enter a NUMBER!")
-        continue
-    
-    else:
-        r = round(d / 2, 2)
-        area = round(math.pi * (r ** 2), 2)
-        circumference = round(2 * math.pi * r, 2)
-    break
-
-print("The diameter of your circle is: " + str(d))
-print("The radius of your circle is: " + str(r))
-print("The area of your circle is: " + str(area))
-print("And the circumference of your circle is: " + str(circumference))
+    continue
+    except d <= 0:
+        print("Please enter a POSITIVE NUMBER!")
+    continue
