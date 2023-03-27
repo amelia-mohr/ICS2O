b256acbc66375913878fd42add11a49c6941d0a3
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-27 17:33:31 +0000

    fix

--- Assignments/Assignment2/assignment2.py
+++ Assignments/Assignment2/assignment2.py
@@ -7,11 +7,12 @@ while not finished:
         if d <= 0:
             raise ValueError
         u = str(input("Please enter a unit (ex. cm, ft, etc.): "))
-        def round_half_up(n, y):
-            multiplier = 10 ** y
-            return (n*multiplier + 0.5) / multiplier
+        def round_half_up(n):
+            
+            n = n + 0.005
+            return round(n, 2)
         r = str(d / 2)
-        print(round_half_up(0.2987654, 2))
+        print(round_half_up(0.001))
         finished = True
     except ValueError:
         print("Please enter a POSITIVE NUMBER!")
