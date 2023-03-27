18c1f9cb50ecb965277206c2f289ece58d3672d5
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-27 14:58:28 +0000

    fix

--- Assignments/Assignment2/assignment2.py
+++ Assignments/Assignment2/assignment2.py
@@ -7,14 +7,10 @@ while not finished:
         if d <= 0:
             raise ValueError
         u = str(input("Please enter a unit (ex. cm, ft, etc.): "))
-        r = round(d / 2, 2)
-        a = round(pi*(r**2), 2)
-        c = round(2*pi*r, 2)
-        print("All results (except for the diameter) are rounded to 2 decimal places.")
-        print("The diameter of your circle is: " + str(d) + " " + u)
-        print("The radius of your circle is: " + str(r) + " " + u)
-        print("The area of your circle is: " + str(a) + " " + u + "²")
-        print("And the circumference of your circle is: " + str(c) + " " + u)
+        r = str(d / 2)
+        if r[len(r)-1] == 5:
+            r = r[len(r)-1] + 1
+            print(r)
         finished = True
     except ValueError:
         print("Please enter a POSITIVE NUMBER!")
