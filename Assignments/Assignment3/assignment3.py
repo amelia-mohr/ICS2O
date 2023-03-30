76feb33f42fa84587f86a0635c995e6dec478c65
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-30 17:38:52 +0000

    fix

--- Assignments/Assignment3/assignment3.py
+++ Assignments/Assignment3/assignment3.py
@@ -35,39 +35,45 @@ while not finished:
             if t == 'yes':
                 t1 = input("Do you want Onions ($1.00) (yes/no)? ")
                 if t1 == 'yes':
-                    price2 = 10.00 + 1.00
+                    price = 10.00 + 1.00
                 else: 
-                    price2 = 10.00
+                    price = 10.00
                 t2 = input("Do you want Pepperoni ($1.75) (yes/no)? ")
                 if t2 == 'yes':
-                    price2 = price2 + 1.75
+                    price = price + 1.75
                 else: 
                     pass
                 t3 = input("Do you want Bacon ($2.50) (yes/no)? ")
                 if t3 == 'yes':
-                    price2 = price2 + 2.50
+                    price = price + 2.50
                 else: 
                     pass
                 t4 = input("Do you want Olives ($3.35) (yes/no)? ")
                 if t4 == 'yes':
-                    price2 = price2 + 3.35
+                    price = price + 3.35
                 else: 
                     pass
             else:
-                price2 = 10.00
+                price = 10.00
     finally:
         decimal.getcontext().rounding = decimal.ROUND_HALF_UP
-        if size == 'L':
-            price = round(decimal.Decimal(price * 1.13), 2)
-            print("Your total is: $" + str(price))
-        elif size == 'XL':
-            price2 = round(decimal.Decimal(price2 * 1.13), 2)
-            print("Your total is: $" + str(price2))
+        p = round(decimal.Decimal(price * 1.13), 2)
+        print("Your pizza is: $" + str(p))
         print("Do you want another pizza?")
         more = input("Answer here (yes/no): ")
         if more == 'yes':
+            price = round(decimal.Decimal(price), 2)
+            pizzas.append(price)
+            for x in pizzas:
+                print("$" + x)
             finished = False
         elif more == 'no':
+            price = round(decimal.Decimal(price), 2)
+            pizzas.append(price)
+            total = sum(pizzas) * 1.13
+            for x in pizzas:
+                print("$" + str(x))
+            print()
             print("Thank you for buying a pizza!")
             finished = True
 
