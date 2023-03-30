65189b3f8dff6240126cf4557f09572ba504938f
amelia-mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-03-30 14:58:00 +0000

    fix

--- Assignments/Assignment3/assignment3.py
+++ Assignments/Assignment3/assignment3.py
@@ -3,52 +3,52 @@ import decimal
 finished = False
 while not finished:
     try: 
-        print("Do you want a Large ($6.00) or Extra Large($10.00) pizza?")
-        size = input("Answer here (Large or XLarge): ")
-        if size == 'Large':
+        print("Do you want a Large ($6.00) or Extra Large ($10.00) pizza?")
+        size = input("Answer here (L or XL): ")
+        if size == 'L':
             t = input("Do you want toppings (yes/no)? ")
             if t == 'yes':
-                t1 = input("Do you want Onions (yes/no)? ")
+                t1 = input("Do you want Onions ($1.00) (yes/no)? ")
                 if t1 == 'yes':
                     price = 6.00 + 1.00
                 else: 
                     price = 6.00
-                t2 = input("Do you want Pepperoni (yes/no)? ")
+                t2 = input("Do you want Pepperoni ($1.75) (yes/no)? ")
                 if t2 == 'yes':
                     price = price + 1.75
                 else: 
                     pass
-                t3 = input("Do you want Bacon (yes/no)? ")
+                t3 = input("Do you want Bacon ($2.50) (yes/no)? ")
                 if t3 == 'yes':
                     price = price + 2.50
                 else: 
                     pass
-                t4 = input("Do you want Olives (yes/no)? ")
+                t4 = input("Do you want Olives ($3.35) (yes/no)? ")
                 if t4 == 'yes':
                     price = price + 3.35
                 else: 
                     pass
             else:
                 price = 6.00
-        elif size == 'XLarge':
+        elif size == 'XL':
             t = input("Do you want toppings (yes/no)? ")
             if t == 'yes':
-                t1 = input("Do you want Onions (yes/no)? ")
+                t1 = input("Do you want Onions ($1.00) (yes/no)? ")
                 if t1 == 'yes':
                     price2 = 10.00 + 1.00
                 else: 
                     price2 = 10.00
-                t2 = input("Do you want Pepperoni (yes/no)? ")
+                t2 = input("Do you want Pepperoni ($1.75) (yes/no)? ")
                 if t2 == 'yes':
                     price2 = price2 + 1.75
                 else: 
                     pass
-                t3 = input("Do you want Bacon (yes/no)? ")
+                t3 = input("Do you want Bacon ($2.50) (yes/no)? ")
                 if t3 == 'yes':
                     price2 = price2 + 2.50
                 else: 
                     pass
-                t4 = input("Do you want Olives (yes/no)? ")
+                t4 = input("Do you want Olives ($3.35) (yes/no)? ")
                 if t4 == 'yes':
                     price2 = price2 + 3.35
                 else: 
@@ -57,10 +57,10 @@ while not finished:
                 price2 = 10.00
     finally:
         decimal.getcontext().rounding = decimal.ROUND_HALF_UP
-        if size == 'Large':
+        if size == 'L':
             price = round(decimal.Decimal(price * 1.13), 2)
             print("Your total is: $" + str(price))
-        elif size == 'XLarge':
+        elif size == 'XL':
             price2 = round(decimal.Decimal(price2 * 1.13), 2)
             print("Your total is: $" + str(price2))
         print("Do you want another pizza?")
