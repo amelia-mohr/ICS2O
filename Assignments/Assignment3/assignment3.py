2e81e8c34f4890a052c16fa63dc8735a4bf84abb
Amelia Mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-04-01 14:24:55 +0000

    fix

--- Assignments/Assignment3/assignment3.py
+++ Assignments/Assignment3/assignment3.py
@@ -1,7 +1,10 @@
-import math
 import decimal
 finished = False
 pizzas = []
+def psize():
+    print()
+
+
 while not finished:
     try: 
         print("Do you want a Large ($6.00) or Extra Large ($10.00) pizza?")
@@ -14,33 +17,23 @@ while not finished:
                     price = 6.00 + 1.00
                 elif t1 == 'no': 
                     price = 6.00
-                else:
-                    raise NameError
                 t2 = input("   Do you want Pepperoni ($1.75) (yes/no)? ")
                 if t2 == 'yes':
                     price = price + 1.75
                 elif t2 == 'no': 
                     pass
-                else:
-                    raise NameError  
                 t3 = input("   Do you want Bacon ($2.50) (yes/no)? ")
                 if t3 == 'yes':
                     price = price + 2.50
                 elif t3 == 'no': 
                     pass
-                else:
-                    raise NameError
                 t4 = input("   Do you want Olives ($3.35) (yes/no)? ")
                 if t4 == 'yes':
                     price = price + 3.35
                 elif t4 == 'no': 
                     pass
-                else:
-                    raise NameError
             elif t == 'no':
                 price = 6.00
-            else:
-                raise NameError
         elif size == 'XL':
             t = input("Do you want toppings (yes/no)? ")
             if t == 'yes':
@@ -49,37 +42,23 @@ while not finished:
                     price = 10.00 + 1.00
                 elif t1 == 'no': 
                     price = 10.00
-                else:
-                    raise NameError
                 t2 = input("   Do you want Pepperoni ($1.75) (yes/no)? ")
                 if t2 == 'yes':
                     price = price + 1.75
                 elif t2 == 'no': 
                     pass
-                else:
-                    raise NameError
                 t3 = input("   Do you want Bacon ($2.50) (yes/no)? ")
                 if t3 == 'yes':
                     price = price + 2.50
                 elif t3 == 'no': 
                     pass
-                else:
-                    raise NameError
                 t4 = input("   Do you want Olives ($3.35) (yes/no)? ")
                 if t4 == 'yes':
                     price = price + 3.35
                 elif t4 == 'no': 
                     pass
-                else:
-                    raise NameError
             elif t == 'no':
                 price = 10.00
-            else:
-                raise NameError
-        else:
-            raise NameError
-    except NameError:
-        print("Please enter an input indicated in the last brackets!")
     try:
         decimal.getcontext().rounding = decimal.ROUND_HALF_UP
         pizzas.append(price)
@@ -120,12 +99,3 @@ while not finished:
                 print("And your total is: $" + str(total))
                 print("Thank you for buying our pizzas!")
                 more = 'done'
-            else:
-                raise ValueError
-            finished = True
-        else:
-            raise NameError
-    except NameError:
-        print("Please enter an input indicated in the last brackets!")
-    except ValueError:
-        print("Please enter the number of the item you would like removed only.")
