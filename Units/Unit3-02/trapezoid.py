640c727e60f3653329dcee2f7bffa7eb44a84945
Amelia Mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-04-05 19:21:06 +0000

    fix

--- Units/Unit3-02/trapezoid.py
+++ Units/Unit3-02/trapezoid.py
@@ -1,9 +1,36 @@
 import decimal
 decimal.getcontext().rounding = decimal.ROUND_HALF_UP
-finished = False
-while not finished:
-    a = int(input("Please enter the length of side a for your trapezoid: "))
-    if 
-    b = int(input("Please enter the length of side b for your trapezoid: "))
-    h = 
 
+def sidea():
+    finished = False
+    while not finished:
+        a = float(input("Please enter a length for base a of your trapezoid: "))
+        if a <= 0:
+            print("Please enter a POSITIVE NUMBER!")
+        else: 
+            finished = True
+    return a
+
+def sideb():
+    finished = False
+    while not finished:
+        b = float(input("Please enter a length for base b of your trapezoid: "))
+        if b <= 0:
+            print("Please enter a POSITIVE NUMBER!")
+        else: 
+            finished = True
+    return b
+
+def height():
+    finished = False
+    while not finished:
+        h = float(input("Please enter a height for your trapezoid: "))
+        if h <= 0:
+            print("Please enter a POSITIVE NUMBER!")
+        else: 
+            finished = True
+    return h
+
+area = ((sidea() + sideb())/2) * height()
+final = round(decimal.Decimal(str(area)), 2)
+print("The area of your trapezoid is: " + str(final))
