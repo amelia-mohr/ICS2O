1c59295303495fdc2cb340d3da89fd6afce73f6a
Amelia Mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-04-05 19:03:37 +0000

    fix

---
+++ Units/Unit3-02/trapezoid.py
@@ -0,0 +1,9 @@
+import decimal
+decimal.getcontext().rounding = decimal.ROUND_HALF_UP
+finished = False
+while not finished:
+    a = int(input("Please enter the length of side a for your trapezoid: "))
+    if 
+    b = int(input("Please enter the length of side b for your trapezoid: "))
+    h = 
+
