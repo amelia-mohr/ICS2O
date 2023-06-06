6e4e77ee386ced4cc9fc23d8d9bc4abd5845188f
Amelia Mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-06-06 14:27:48 +0000

    fix

--- finalproject/js/cartscript.js
+++ finalproject/js/cartscript.js
@@ -2,9 +2,13 @@ const btnCart = document.querySelector('.bag');
 const cart = document.querySelector('.cart');
 const btnClose = document.querySelector('#cart-close');
 
-btnCart.addEventListener('click', () => {
-    cart.classList.add('cart-active');
+btnCart.forEach((btn) => {
+    btn.addEventListener('click', showCart);
 });
+
+function showCart(){
+    cart.classList.add('cart-active');
+};
 
 btnClose.addEventListener('click', () => {
     cart.classList.remove('cart-active');
