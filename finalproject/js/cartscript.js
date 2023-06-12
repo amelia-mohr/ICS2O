3c39f5dc18286b6adeb4d59d42743a374eb5862b
Amelia Mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-06-12 13:44:19 +0000

    fixed image link

--- finalproject/js/cartscript.js
+++ finalproject/js/cartscript.js
@@ -91,7 +91,7 @@ function createCartProduct(title,qty,titleToPrice,titleToImage){
     
     return `
     <div class="cart-box">
-      <img src="/images/${image}" class="cart-img">
+      <img src="/finalproject/images/${image}" class="cart-img">
       <div class="detail-box">
         <h3>${title}</h3>
         <div class="price-box">
