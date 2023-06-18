248bf47531c32e7916f557931695ca8a120d9dc7
Amelia Mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-06-18 13:13:09 +0000

    prepping required btn to send cookie as JSON

---
+++ finalproject/js/continue.js
@@ -0,0 +1,55 @@
+const sendBtn = document.getElementById('send');
+const fName = document.getElementById('fName');
+const lName = document.getElementById('lName');
+
+// title to price
+var titleToPrice = {};
+titleToPrice['Product1'] = 7.00;
+titleToPrice['Product2'] = 6.00;
+titleToPrice['Product3'] = 5.00;
+titleToPrice['Product4'] = 20.00;
+
+// title to image
+var titleToImage = {};
+titleToImage['Product1'] = "whale/dark_blue_front.jpeg";
+titleToImage['Product2'] = "egg/one_egg.jpeg";
+titleToImage['Product3'] = "bubbletea/bubbletea_1.jpeg";
+titleToImage['Product4'] = "octopus/octopus_front.jpeg";
+
+// title to product name
+var titleToProduct = {};
+titleToProduct['Product1'] = "Whale";
+titleToProduct['Product2'] = "Sunny Side Up Eggs";
+titleToProduct['Product3'] = "Bubble Tea";
+titleToProduct['Product4'] = "Octopus";
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            nc = c.substring(1);
+        };
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        };
+    };
+    return null;
+}
+
+sendBtn.addEventListener('click', () => {
+    if (fName.value == null || lName.value == null) {
+        return;
+    }
+
+    let c = getCookie("cart");
+    if (c == null) {
+        // nothing in the cookie, cart is empty
+        return;
+    } else {
+        let obj = JSON.parse(c);
+        console.log(obj);
+    };
+});
